64dcc387cd41cfeb1a02a42baeeba690c586ed34
oitsjustjose <stovallj1995@gmail.com>
2018-12-04 18:55:54 -0500

    Improve connection checking and reroute

--- scal.js
+++ scal.js
@@ -40,8 +40,7 @@ function hideVideo(element) {
     var hidden = video.style.display === "none" ? true : false;
     if (!hidden) {
         $(video).hide("slow")
-    }
-    else {
+    } else {
         $(video).show("slow")
     }
     hidden = !hidden;
@@ -49,3 +48,91 @@ function hideVideo(element) {
     element.innerText = hidden ? "Show Live Stream" : "Hide Live Stream";
 }
 
+function checkConnection() {
+    $.ajax({
+        url: "http://150.182.130.194:3030/video_feed",
+        type: "HEAD",
+        timeout: 1000,
+        statusCode: {
+            // Can connect
+            200: function (response) {
+                console.log(200)
+                $('#counting_feed_err').hide()
+            },
+            // Can't connect
+            400: function (response) {
+                $.ajax({
+                    url: "http://10.199.1.152:3030/video_feed",
+                    type: "HEAD",
+                    statusCode: {
+                        200: function (response) {
+                            console.log(400, 200)
+                            // Can connect, do something with embed() replace
+                            $('#counting_feed_err').hide()
+                            document.getElementById("video").setAttribute("data-url", "http://10.199.1.152:3030/video_feed")
+                            $('#video').embed()
+                        },
+                        400: function (response) {
+                            console.log(400, 400)
+                            // Really can't connect.
+                            $('#video_feed').hide()
+                            $('#video').embed('destroy')
+                        },
+                        0: function (response) {
+                            // Can connect
+                            console.log(400, 0)
+                            if (response.statusText === "error") {
+                                $('#counting_feed_err').hide()
+                            } else {
+                                $('#video_feed').hide()
+                                $('#video').embed('destroy')
+                            }
+                        }
+                    }
+                });
+            },
+            // Maybe can connect?
+            0: function (response) {
+                // Can connect
+                if (response.statusText === "error") {
+                    $('#counting_feed_err').hide()
+                } else {
+                    $.ajax({
+                        url: "http://10.199.1.152:3030/video_feed",
+                        type: "HEAD",
+                        statusCode: {
+                            200: function (response) {
+                                console.log(0, 200)
+                                // Can connect, do something with embed() replace
+                                $('#counting_feed_err').hide()
+                                document.getElementById("video").setAttribute("data-url", "http://10.199.1.152:3030/video_feed")
+                                $('#video').embed()
+                            },
+                            400: function (response) {
+                                console.log(0, 400)
+                                // Really can't connect.
+                                console.log("Error 400")
+                                $('#video_feed').hide()
+                                $('#video').embed('destroy')
+                            },
+                            0: function (response) {
+                                console.log(0, 0)
+                                // Can connect
+                                if (response.statusText === "error") {
+                                    $('#counting_feed_err').hide()
+                                    document.getElementById("video").setAttribute("data-url", "http://10.199.1.152:3030/video_feed")
+                                    $('#video').embed()
+                                    console.log("changing to internal ip")
+                                } else {
+                                    console.log("ho")
+                                    $('#video_feed').hide()
+                                    $('#video').embed('destroy')
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+}
