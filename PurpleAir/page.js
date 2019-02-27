b75e85c572df18511275b643e9f650bb84a48018
oitsjustjose <stovallj1995@gmail.com>
2019-02-27 11:42:42 -0500

    Update to use python consumer server API

--- PurpleAir/page.js
+++ PurpleAir/page.js
@@ -1,9 +1,10 @@
-var vals = []
+var vals = {}
 var times = []
 var time_to_datetime = {}
 var datetimes = []
 
 function init() {
+
     $(document)
         .ready(function () {
             $('.masthead')
@@ -18,73 +19,121 @@ function init() {
                 });
         });
 
-    $.getJSON("https://www.purpleair.com/json?show=14561", function (data) {
-        vals.push(parseFloat(data["results"][0]["PM2_5Value"]))
-        times.push(data["results"][0]["LastSeen"])
-        time_to_datetime[(data["results"][0]["LastSeen"])] = new Date()
-        datetimes.push(new Date())
-
-    })
-
-    var data = [{
-        x: datetimes,
-        y: vals,
-        mode: 'lines+markers',
-        name: 'PM 2.5 Time Series',
-        line: {
-            shape: 'hvh'
-        }
-    }]
-
-    var layout = {
-        title: {
-            text: "PM 2.5 at the Intersection of MLK & Peeples",
-            font: {
-                color: "#EEEEEE"
-            }
-        },
-        plot_bgcolor: "#202020",
-        paper_bgcolor: "#202020",
-        text_color: "#EEEEEE",
-        xaxis: {
-            title: 'Date & Time of Reading',
-            showgrid: false,
-            zeroline: false,
-            color: "#EEEEEE"
-        },
-        yaxis: {
-            title: 'PM 2.5 Reading',
-            showline: false,
-            color: "#EEEEEE"
-        }
-    }
-
-    Plotly.plot(document.getElementById("pa_plot"), data, layout)
-
-    setInterval(() => {
-        // Repeated every minute:
-        $.getJSON("https://www.purpleair.com/json?show=14561", function (data) {
-            vals.push(parseFloat(data["results"][0]["PM2_5Value"]))
-            times.push(data["results"][0]["LastSeen"])
-            time_to_datetime[(data["results"][0]["LastSeen"])] = new Date()
-            datetimes.push(new Date())
-            // document.getElementById("pa_plot").innerHTML = ""
-            data = [{
-                x: datetimes,
-                y: vals,
-                mode: 'lines+markers',
-                name: 'PM 2.5 Time Series',
-                line: {
-                    shape: 'hvh'
-                }
-            }]
-
-            Plotly.react(document.getElementById("pa_plot"), data, layout)
-        })
-    }, 1000);
+    getIP();
 }
 
+function getIP() {
+    function setIP(url) {
 
+
+        $.getJSON(url, function (data) {
+            console.log(data);
+        });
+
+        var data = [{
+            x: datetimes,
+            y: vals,
+            mode: 'lines+markers',
+            name: 'PM 2.5 Time Series',
+            line: {
+                shape: 'hvh'
+            }
+        }];
+
+        var layout = {
+            title: {
+                text: "PM 2.5 at the Intersection of MLK & Peeples",
+                font: {
+                    color: "#EEEEEE"
+                }
+            },
+            plot_bgcolor: "#202020",
+            paper_bgcolor: "#202020",
+            text_color: "#EEEEEE",
+            xaxis: {
+                title: 'Date & Time of Reading',
+                showgrid: false,
+                zeroline: false,
+                color: "#EEEEEE"
+            },
+            yaxis: {
+                title: 'PM 2.5 Reading',
+                showline: false,
+                color: "#EEEEEE"
+            }
+        };
+
+        Plotly.plot(document.getElementById("pa_plot"), data, layout);
+
+        // setInterval(() => {
+        //     // Repeated every minute:
+        // }, 1000);
+        // document.getElementById("ip_storage").innerText = ip;
+    }
+    $.ajax({
+        url: "http://150.182.130.194:3100/api",
+        type: "HEAD",
+        timeout: 1000,
+        statusCode: {
+            // Can connect
+            200: function (response) {
+                setIP("http://150.182.130.194:3100/api");
+            },
+            // Can't connect
+            400: function (response) {
+                $.ajax({
+                    url: "http://10.199.1.152:3100/api",
+                    type: "HEAD",
+                    statusCode: {
+                        200: function (response) {
+                            setIP("http://10.199.1.152:3100/api");
+                        },
+                        400: function (response) {
+                            setIP(null);
+                        },
+                        0: function (response) {
+                            // Can connect
+                            if (response.statusText === "error") {
+                                setIP("http://10.199.1.152:3100/api");
+                            } else {
+                                setIP(null);
+                            }
+
+                        }
+                    }
+                });
+            },
+            // Maybe can connect?
+            0: function (response) {
+                // Can connect
+                if (response.statusText === "error") {
+                    setIP("http://150.182.130.194:3100/api");
+                } else {
+                    $.ajax({
+                        url: "http://10.199.1.152:3100/api",
+                        type: "HEAD",
+                        timeout: 1000,
+                        statusCode: {
+                            200: function (response) {
+                                setIP("http://10.199.1.152:3100/api");
+                            },
+                            400: function (response) {
+                                setIP(null);
+                            },
+                            0: function (response) {
+                                if (response.statusText === "error") {
+                                    setIP("http://10.199.1.152:3100/api");
+                                } else {
+                                    setIP(null);
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    });
+}
 
 function toggleSidebar() {
     $('.ui.sidebar').sidebar('toggle');
