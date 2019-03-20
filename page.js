b2b6f3d7de6fdbf405624ddb06563e1e97eccfee
oitsjustjose <stovallj1995@gmail.com>
2019-03-20 00:56:09 -0400

    Initial commit of new style

--- page.js
+++ page.js
@@ -1,18 +1,21 @@
-function init() {
-    $(document)
-        .ready(function () {
-            $('.masthead')
-                .visibility({
-                    once: false,
-                    onBottomPassed: function () {
-                        $('.fixed.menu').transition('fade in');
-                    },
-                    onBottomPassedReverse: function () {
-                        $('.fixed.menu').transition('fade out');
-                    }
-                });
-        });
-}
+$(document)
+    .ready(function () {
+        $('.masthead')
+            .visibility({
+                once: false,
+                onBottomPassed: function () {
+                    $('.fixed.menu').transition('fade in');
+                },
+                onBottomPassedReverse: function () {
+                    $('.fixed.menu').transition('fade out');
+                }
+            });
+
+        // $('img').hide();
+        // $('img').show("slow");
+    });
+
+
 
 function toggleSidebar() {
     $('.ui.sidebar').sidebar('toggle');
