b7ccb6c2d483bede9c510de2c7e838d34b4aa163
oitsjustjose <stovallj1995@gmail.com>
2019-03-20 10:40:20 -0400

    Match squarespace color theme

--- page.js
+++ page.js
@@ -1,15 +1,5 @@
 $(document)
     .ready(function () {
-        $('.masthead')
-            .visibility({
-                once: false,
-                onBottomPassed: function () {
-                    $('.fixed.menu').transition('fade in');
-                },
-                onBottomPassedReverse: function () {
-                    $('.fixed.menu').transition('fade out');
-                }
-            });
 
         $('.ui.sidebar').sidebar({
             transition: 'overlay'
